0407622577f84363c9704d00214b534a48ef77a6
sjller <1109925966@qq.com>
2013-10-05 20:19:09 +0800

    first time

---
+++ src/FirstObject.java
@@ -0,0 +1,20 @@
+import javax.xml.ws.WebServiceProvider;
+
+
+public class FirstObject {
+	/**
+	 * @author Administrator
+	 * @param aa
+
+	 */	
+			
+			public static void main(String[] args) {
+				/**
+				 * 我爱你；
+				 */
+				System.getProperties().list(System.out);
+				System.out.println("*********************");
+				System.out.println(System.getProperty("user.name"));
+				System.out.println(System.getProperty("java.library.path"));
+			}
+}
